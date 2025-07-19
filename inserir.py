01d697e1e6de9a7398bb29c3413a2ca0db0693c0
ElissonXD <elissondias2006@gmail.com>
2025-07-18 22:10:09 -0300

    inserir atualizado

--- inserir.py
+++ inserir.py
@@ -9,15 +9,13 @@ resposta = input('qual tabela: ')
 if resposta == "treinador":
     id_treinador = input("insira o id: ")
     nome_treinador = input("insira o nome: ")
-    indice_pokemon = int(input("Insira o indice de um dos Pokémon do treinador: "))
-    especie_pokemon = input("Insira a espécie do Pokémon do treinador: ")
+    numero_pokemon = int(input("Insira o numero de um dos Pokémon do treinador: "))
+    id_pokemon = int(input("Insira a id do Pokémon do treinador: "))
 
-    
-    #TODO: Consulta se o pokémon existe
     cursor.execute("""
     SELECT * FROM pokemon p
-                   WHERE p.indice = ? AND p.especie = ?
-""", indice_pokemon, especie_pokemon)
+                   WHERE p.numero = ? AND p.id_pokemon = ?
+""", (numero_pokemon, id_pokemon))
     pokemon_exists = bool(cursor.fetchall())
 
     if pokemon_exists:
@@ -26,8 +24,25 @@ if resposta == "treinador":
         VALUES (?, ?)
         """, (id_treinador, nome_treinador))
 
-        #TODO: Adicionar o ID do treinador como estrangeira no pokémon escolhido
-        ...
+        cursor.execute("""
+        UPDATE pokemon
+        SET id_treinador = ?
+        WHERE numero = ? AND id_pokemon = ?
+        """, (id_treinador, numero_pokemon, id_pokemon))
+    else:
+        print("valor não inserido :)")
+
+elif resposta == "pokemon":
+    numero_pokemon = int(input("insira o numero do pokemon: "))
+    id_pokemon = int(input("insira o id do pokemon: "))
+    rota_pokemon = input("insira a rota onde foi encontrado: ")
+    região_pokemon = input("insir a região onde o pokemon foi encontrado: ")
+
+    cursor.execute("""
+    INSERT INTO pokemon (numero, id_pokemon, rota, regiao)
+    VALUES (?, ?, ?, ?)
+    """, (numero_pokemon, id_pokemon,rota_pokemon, região_pokemon))
+
 elif resposta == "professor":
     id_treinador = input("insira o id: ")
     area_pesquisa = input("insira a area de pesquisa: ")
