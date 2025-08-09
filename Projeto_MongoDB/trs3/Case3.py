f12e857b89da2483cefcf9c199e6a27d3f28ba77
Thawan <163412559+thawan-xyz@users.noreply.github.com>
2025-08-09 20:21:46 -0300

    case3

--- Projeto_MongoDB/trs3/Case3.py
+++ Projeto_MongoDB/trs3/Case3.py
@@ -1,6 +1,4 @@
-# PokeMove_Case3.py
 # Relacionamento 1:N: cada "pokemon" guarda um ARRAY de _ids de "movimento"
-# Consulta: nomes dos movimentos do Pokémon com nome = <entrada>
 import pymongo
 
 cliente = pymongo.MongoClient("mongodb://localhost:27017/")
